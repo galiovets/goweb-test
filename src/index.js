45384cc6e2071c293e20856070bb8fd5ecbfa51c
galiovets <m.galiovets@gmail.com>
2022-09-29 20:01:47 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
-// import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyles } from 'styles';
 import App from './App';
-
-// const root = createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyles />
-//       <App />
-//     </ThemeProvider>
-//   </React.StrictMode>,
-// );
 
 ReactDOM.render(
   <React.StrictMode>
